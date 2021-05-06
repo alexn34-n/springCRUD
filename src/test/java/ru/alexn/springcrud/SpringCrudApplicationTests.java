ff8afb37196935d1b767f4f347f7f846599f6ecd
alexn34-n <alexn3469@mail.ru>
2021-05-06 17:09:31 +0300

    Initial commit

---
+++ src/test/java/ru/alexn/springcrud/SpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alexn.springcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
